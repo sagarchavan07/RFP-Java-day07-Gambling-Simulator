ddbad981fc8c59c0df30b3c15fd25657cc3eaa9d
sagarchavan07 <chavansagar96044@gmail.com>
2022-07-13 21:25:41 +0530

    starts with 100 dollar stake and 1 dollar bet

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,4 +1,14 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
+    public static void main(String[] args) {
+        for (int day = 1; day <= 20; day++) {
+            int dayStake=100;
+            doBet(1);
+        }
+    }
+    static int doBet(int bid){
+
+        return 0;
+    }
 }
